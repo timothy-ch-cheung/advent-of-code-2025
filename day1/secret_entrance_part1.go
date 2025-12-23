f5f91c929612af56897a5c01433ec9310843d4f9
Tim Cheung <43986931+timothy-ch-cheung@users.noreply.github.com>
2025-12-23 01:13:36 +0000

    Add solution for day 1 part 1

---
+++ day1/secret_entrance_part1.go
@@ -0,0 +1,90 @@
+package day1
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Direction int
+
+const (
+	Left Direction = iota
+	Right
+)
+
+type instruction struct {
+	direction Direction
+	rotations int
+}
+
+func SolvePart1(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	solution := 0
+	currentPos := 50
+
+	for scanner.Scan() {
+		currInstruction := parseLine(scanner.Text())
+		switch currInstruction.direction {
+		case Left:
+			currentPos = rotateLeft(currentPos, currInstruction.rotations)
+		case Right:
+			currentPos = rotateRight(currentPos, currInstruction.rotations)
+		}
+		if currentPos == 0 {
+			solution += 1
+		}
+	}
+
+	fmt.Println(solution)
+}
+
+func rotateLeft(currPos int, rotation int) int {
+	rotation = rotation % 100
+	diff := currPos - rotation
+	if diff < 0 {
+		currPos = 100 + diff
+	} else {
+		currPos = diff
+	}
+	return currPos
+}
+
+func rotateRight(currPos int, rotation int) int {
+	rotation = rotation % 100
+	sum := currPos + rotation
+	if sum < 99 {
+		currPos = sum
+	} else {
+		currPos = sum - 100
+	}
+	return currPos
+}
+
+func parseLine(line string) instruction {
+	var direction Direction
+	if line[0] == 'R' {
+		direction = Right
+	} else if line[0] == 'L' {
+		direction = Left
+	} else {
+		panic("Invalid direction code")
+	}
+
+	rotations, err := strconv.Atoi(line[1:])
+	if err != nil {
+		panic("Invalid rotation code")
+	}
+
+	return instruction{
+		direction: direction,
+		rotations: rotations,
+	}
+}
